6554e6fbb9a0e1e1a6c0f9e5017d73e817d8daa5
Jagadthilak <jagadthilak00@gmail.com>
2024-08-16 19:35:50 +0530

    added procfile for render deployment

--- CRUDApplicationProjectBackend/src/main/java/com/dev/application/config/WebConfig.java
+++ CRUDApplicationProjectBackend/src/main/java/com/dev/application/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("https://crud-page.netlify.app")
 		.allowedMethods("GET","POST","PUT","DELETE")
 		.allowedHeaders("*");
 		
